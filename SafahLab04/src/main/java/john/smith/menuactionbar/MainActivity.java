01fcae059e071f55138ad72b990f73e809e04a65
Safahvirk6470 <n01596470@humbermail.ca>
2024-06-01 14:39:21 -0400

    2 screens

--- SafahLab04/src/main/java/john/smith/menuactionbar/MainActivity.java
+++ SafahLab04/src/main/java/john/smith/menuactionbar/MainActivity.java
@@ -1,67 +1,49 @@
-package john.smith.menuactionbar;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import john.smith.menuactionbar.R;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int featureId;
-    private MenuItem item;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editText = findViewById(R.id.editText);
+
+        ImageButton button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = editText.getText().toString();
+                long data = input.isEmpty() ? -2000 : Long.parseLong(input);
+
+                // Show snackbar with indefinite duration
+                Snackbar snackbar = Snackbar.make(v, "John Smith", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("START", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Navigate to second activity
+                               // Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                                //intent.putExtra("data", data);
+                                //startActivity(intent);
+
+                                // Clear input
+                                editText.setText("");
+                            }
+                        });
+                snackbar.show();
+            }
         });
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the tools bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.start) {
-            //displayTextView.setText("You selected start!");
-            Toast.makeText(this, "You selected start!", Toast.LENGTH_LONG).show();
-        }
-
-        if (item.getItemId() == R.id.accel) {
-
-            //displayTextView.setText("You selected start!");
-            Toast.makeText(this, "You selected Speed up!!", Toast.LENGTH_LONG).show();
-        }
-
-        if (item.getItemId() == R.id.stop) {
-
-            //displayTextView.setText("You selected start!");
-            Toast.makeText(this, "You selected stop!", Toast.LENGTH_LONG).show();
-        }
-
-        if (item.getItemId() == R.id.decel) {
-
-            //displayTextView.setText("You selected start!");
-            Toast.makeText(this, "You selected Slow Down!", Toast.LENGTH_LONG).show();
-        }
-
-        return false;
-    }
-
-
 }
